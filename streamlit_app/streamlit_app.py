1b196eac4e04894277dae228fd82ad958eef12cf
CristianBMJ <cpmartnez@ug.uchile.cl>
2025-03-26 10:01:22 -0300

    choose input format

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -3,20 +3,47 @@ import requests
 
 st.title("Predictive Maintenance Model")
 
-# Initialize a list to hold the features
-features = []
-
-# Input fields for features
-for i in range(1, 13):
-    feature_value = st.number_input(f"Feature {i}")  # Get input for each feature
-    features.append(feature_value)  # Append the input value to the features list
-
-if st.button("Predict"):
-    # Prepare the data for the API
-    response = requests.post("http://127.0.0.1:5001/predict", json={'features': features})
+option = st.selectbox("Choose input method:", ["Manual Input", "Select Row", "List"])
     
-    if response.status_code == 200:
-        prediction = response.json()['prediction']
-        st.success(f"Prediction: {prediction}")
-    else:
-        st.error("Error in prediction")
+if option == "Manual Input":
+    features = st.text_input("Enter 12 float features as comma-separated values:")
+    st.write( features ) 
+    st.write( features.split(',')[0].strip() ) 
+
+    if st.button("Predict"):
+        features = [float(x.strip() ) for x in features.split( ',') ]
+
+        # Call your prediction function here
+        #prediction = model.predict([features_list])
+        response = requests.post("http://127.0.0.1:5001/predict", json={'features': features})
+        
+        if response.status_code == 200:
+            prediction = response.json()['prediction']
+            st.success(f"Prediction: {prediction}")
+        else:
+            st.error("Error in prediction:")
+        st.write(f"Prediction: {prediction}")
+
+elif option == "Select Row":
+    # Implement logic to select a row from a dataset
+    # For example, using st.selectbox or st.dataframe
+    pass  # Replace with actual implementation
+
+elif option == "List":
+    # Initialize a list to hold the features
+    features = []
+
+    # Input fields for features
+    for i in range(1, 13):
+        feature_value = st.number_input(f"Feature {i}")  # Get input for each feature
+        features.append(feature_value)  # Append the input value to the features list
+
+    if st.button("Predict"):
+        # Prepare the data for the API
+        response = requests.post("http://127.0.0.1:5001/predict", json={'features': features})
+        
+        if response.status_code == 200:
+            prediction = response.json()['prediction']
+            st.success(f"Prediction: {prediction}")
+        else:
+            st.error("Error in prediction")
